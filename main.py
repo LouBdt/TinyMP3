a00e04429f9310a2d353e18493b78ce616218ef8
LouBdt <81627775+LouBdt@users.noreply.github.com>
2023-03-21 16:09:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ Created on Mon Mar 20 15:30:25 2023
 """
 
 import scrapetube
+import re
+import itertools
 from yt_dlp import YoutubeDL
 videos = scrapetube.get_channel("UC4eYXhJI4-7wSWc8UNRwD4A")
-
+videos1, videos2 = itertools.tee(videos)
 ydl_opts = {
                 'format': 'bestaudio/best',
                 'postprocessors': [{
@@ -19,7 +21,18 @@ ydl_opts = {
                         }],
                     }
                 
- for video in videos:
+
+
+
+
+import os
+root_dir = ""
+data_files = os.listdir()
+tot = len([j for j in videos2])
+print(tot)
+count = 0
+for video in videos1:
+    
     with YoutubeDL(ydl_opts) as ydl:
       info_dict = ydl.extract_info("https://www.youtube.com/watch?v="+video["videoId"], download=False)
       video_url = info_dict.get("url", None)
@@ -31,8 +44,19 @@ ydl_opts = {
     while i< len(video_desc)-1:
         if video_desc[i].lower()=="set" and video_desc[i+1].lower()=="list":
             tiny = True
+            count +=1
             break
         i+=1
-    if tiny:
+    already_down = False
+    for n in data_files:
+        if re.split(":|：",video_title)[0] in re.split(":|：",n)[0]:
+            print(n+": Already downloaded")
+            already_down=True
+            break
+    
+    
+    if tiny and not already_down:
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download("https://www.youtube.com/watch?v="+video["videoId"])
+    print(str(count)+" concerts already downloaded.")
+    print("About "+str(int(100*count/tot))+"% done")
